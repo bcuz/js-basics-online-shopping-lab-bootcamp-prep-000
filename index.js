f9559a0419aa6ba281af1bcfa7e6c1f8c1f8ed34
adam N <adam.nawroc@gmail.com>
2018-02-21 19:49:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ function viewCart() {
     // for (var i = 0; i < cart.length; i++) {
     var key = Object.keys(cart[i])[0]
      if (cart.length === 1) {
-      //  
-      console.log(`In your cart, you have ${key} at $${cart[i][key]}.`)
+      //
+      console.log(`In your cart, you have ${Object.keys(cart[0])[0]} at $${cart[i][key]}.`)
     } else if (cart.length === 2) {
       console.log(`In your cart, you have ${key} at $${cart[i][key]}.`)
     } else {
