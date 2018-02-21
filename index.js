8fe82b92d32b7dc2f9939cb330161f96b0c9422d
adam N <adam.nawroc@gmail.com>
2018-02-21 20:20:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ function placeOrder(cardNumber) {
   if (cardNumber === undefined) {
       console.log("Sorry, we don't have a credit card on file for you.")
   } else {
-    console.log(`Your total cost is $${total()}, which will be charged to the card 83296759.`)
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
   }
 }
 // addToCart("yams");
