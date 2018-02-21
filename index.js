da3b6d4617624884c940c88dd5e340d139e4f93d
adam N <adam.nawroc@gmail.com>
2018-02-21 20:18:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -76,13 +76,13 @@ function removeFromCart(item) {
     if (cart[i].hasOwnProperty(item)) {
       // delete cart[i]
       cart.splice(i, 1)
+      return cart
 
-    } else {
-      console.log("That item is not in your cart.")
     }
 
   }
-  return cart
+  console.log("That item is not in your cart.")
+  
 }
 
 function placeOrder(cardNumber) {
