624bb139640ca2f813ce5935bb4dfec4aa0932a5
adam N <adam.nawroc@gmail.com>
2018-02-21 20:11:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -75,6 +75,8 @@ function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++) {
     if (cart[i].hasOwnProperty(item)) {
       delete cart[i][item]
+    } else {
+      console.log('That item is not in your cart.')
     }
   }
 }
