c42c72170cdbf43704f1656c427524ca6bbc75d3
adam N <adam.nawroc@gmail.com>
2018-02-21 19:41:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function viewCart() {
     for (var i = 0; i < cart.length; i++) {
     var key = Object.keys(cart[i][0])
      if (cart.length === 1) {
-      console.log(`In your cart, you have ${key} at $${}.`)
+      console.log(`In your cart, you have ${key} at $${cart[i][key]}.`)
     } else if (cart.length === 2) {
 
     } else {
