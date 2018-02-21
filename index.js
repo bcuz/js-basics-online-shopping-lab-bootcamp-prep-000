7738fd8eecdc2f18c8faf48e1e1dac21c43472e5
adam N <adam.nawroc@gmail.com>
2018-02-21 19:40:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function viewCart() {
     for (var i = 0; i < cart.length; i++) {
 
      if (cart.length === 1) {
-      console.log(`In your cart, you have ${cart.} at $17.`)
+      console.log(`In your cart, you have ${Object.keys(cart[i])} at $17.`)
     } else if (cart.length === 2) {
 
     } else {
