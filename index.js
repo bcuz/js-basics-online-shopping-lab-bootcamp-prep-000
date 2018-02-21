38db535c1b53996e2080846fcbe4bdec317d664f
adam N <adam.nawroc@gmail.com>
2018-02-21 20:15:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -74,7 +74,8 @@ function removeFromCart(item) {
   // write your code here
   for (var i = 0; i < cart.length; i++) {
     if (cart[i].hasOwnProperty(item)) {
-      delete cart[i]
+      // delete cart[i]
+      cart.splice(i, 1)
 
     } else {
       console.log('That item is not in your cart.')
